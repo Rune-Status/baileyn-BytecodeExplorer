96e09c419d255022ef00940ce7ecb4dac97fcf4b
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2019-11-28 01:06:30 -0500

    Display line numbers if they're available.

--- src/main/java/com/njbailey/explorer/Main.java
+++ src/main/java/com/njbailey/explorer/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        root.getStylesheets().add(getClass().getResource("/main.css").toExternalForm());
 
         Scene scene = new Scene(root);
 
--- src/main/java/com/njbailey/explorer/controls/InstructionPane.java
+++ src/main/java/com/njbailey/explorer/controls/InstructionPane.java
@@ -5,23 +5,33 @@ import com.njbailey.bytelib.OpcodeInfo;
 import com.njbailey.bytelib.code.FrameInstruction;
 import com.njbailey.bytelib.code.Instruction;
 import com.njbailey.bytelib.code.LabelInstruction;
-import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tooltip;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import lombok.Getter;
+
+import java.util.Optional;
 
 public class InstructionPane extends ScrollPane {
     @Getter
     private Method method;
 
+    private VBox lineNumbers = new VBox();
     private VBox instructions = new VBox();
 
     public InstructionPane(Method method) {
         setFitToWidth(true);
         setFitToHeight(true);
-        setContent(instructions);
+
+        lineNumbers.getStyleClass().add("line-numbers");
+        instructions.getStyleClass().add("instructions");
+
+        HBox contents = new HBox();
+        contents.getChildren().addAll(lineNumbers, instructions);
+        setContent(contents);
 
         setMethod(method);
     }
@@ -30,6 +40,7 @@ public class InstructionPane extends ScrollPane {
         this.method = method;
 
         for(Instruction instruction : method.getInstructions()) {
+            Node node;
             if(instruction instanceof LabelInstruction) {
                 addLabelInstruction((LabelInstruction) instruction);
             } else if(instruction instanceof FrameInstruction) {
@@ -47,19 +58,32 @@ public class InstructionPane extends ScrollPane {
             System.out.println("Finished editting label.");
         });
 
-        this.instructions.getChildren().add(editableLabel);
+        HBox hbox = new HBox();
+        Label lineNumberLabel = new Label();
+        lineNumberLabel.getStyleClass().add("line-number-label");
+
+        Optional<Integer> lineNumber = method.getLineNumbers().get(instruction);
+
+        lineNumber.ifPresent(integer -> lineNumberLabel.setText(integer.toString()));
+
+        lineNumbers.getChildren().add(lineNumberLabel);
+        instructions.getChildren().add(editableLabel);
     }
 
     private void addFrameInstruction(FrameInstruction instruction) {
-        System.out.println("Add FrameInstruction");
         Label label = new Label(instruction.toString());
         label.setStyle("-fx-background-color: #FFC0C0");
-        this.instructions.getChildren().add(label);
+
+        lineNumbers.getChildren().add(new Label());
+        instructions.getChildren().add(label);
     }
 
     private void addInstruction(Instruction instruction) {
         Label label = new Label(instruction.toString());
+        label.getStyleClass().add("indent");
         label.setTooltip(new Tooltip(OpcodeInfo.OPCODE_DESCRIPTIONS[instruction.getOpcode()]));
-        this.instructions.getChildren().add(label);
+
+        lineNumbers.getChildren().add(new Label());
+        instructions.getChildren().add(label);
     }
 }
