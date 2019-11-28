95d3dfbd2a28d58cd5b3cad0c43e531940d7d207
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2019-11-28 03:45:37 -0500

    Add designator to determine if a class is abstract, interface, or normal from the TreeView.

--- src/main/java/com/njbailey/explorer/tree/ClassFileTreeItem.java
+++ src/main/java/com/njbailey/explorer/tree/ClassFileTreeItem.java
@@ -1,10 +1,21 @@
 package com.njbailey.explorer.tree;
 
 import com.njbailey.bytelib.ClassFile;
+import javafx.scene.control.Label;
+
+import java.lang.reflect.Modifier;
 
 public class ClassFileTreeItem extends SimpleTreeItem<ClassFile> {
     public ClassFileTreeItem(ClassFile classFile) {
         super(classFile);
         setValue(classFile.getName());
+
+        if(Modifier.isAbstract(classFile.getAccess())) {
+            setGraphic(new Label("[A]"));
+        } else if(Modifier.isInterface(classFile.getAccess())) {
+            setGraphic(new Label("[I]"));
+        } else {
+            setGraphic(new Label("[C]"));
+        }
     }
 }
