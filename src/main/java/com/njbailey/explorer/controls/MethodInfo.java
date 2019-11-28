c6f2fa8ccf3f514962fda73e5e1786286e1f796b
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2019-11-28 16:08:20 -0500

    Start working on a control to display method information.

---
+++ src/main/java/com/njbailey/explorer/controls/MethodInfo.java
@@ -0,0 +1,40 @@
+package com.njbailey.explorer.controls;
+
+import com.njbailey.bytelib.Method;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+import java.lang.reflect.Modifier;
+
+public class MethodInfo extends GridPane {
+    private Method method;
+
+    public MethodInfo(Method method) {
+        this.method = method;
+
+        Label nameLabel = new Label("Name:");
+        EditableLabel nameEditableLabel = new EditableLabel(method.getName());
+        GridPane.setConstraints(nameLabel, 0, 0, 2, 1);
+        GridPane.setConstraints(nameEditableLabel, 2, 0, 2, 1);
+
+        CheckBox privateCheckbox = new CheckBox("Private");
+        privateCheckbox.setSelected(Modifier.isPrivate(method.getAccess()));
+
+        CheckBox publicCheckbox = new CheckBox("Public");
+        publicCheckbox.setSelected(Modifier.isPublic(method.getAccess()));
+
+        CheckBox synchronizedCheckbox = new CheckBox("Synchronized");
+        synchronizedCheckbox.setSelected(Modifier.isSynchronized(method.getAccess()));
+
+        CheckBox finalCheckbox = new CheckBox("Final");
+        finalCheckbox.setSelected(Modifier.isFinal(method.getAccess()));
+
+        GridPane.setConstraints(privateCheckbox, 0, 1);
+        GridPane.setConstraints(publicCheckbox, 1, 1);
+        GridPane.setConstraints(synchronizedCheckbox, 2, 1);
+        GridPane.setConstraints(finalCheckbox, 3, 1);
+
+        getChildren().addAll(nameLabel, nameEditableLabel, privateCheckbox, publicCheckbox, synchronizedCheckbox, finalCheckbox);
+    }
+}
--- src/main/java/com/njbailey/explorer/tree/MethodTreeItem.java
+++ src/main/java/com/njbailey/explorer/tree/MethodTreeItem.java
@@ -1,8 +1,11 @@
 package com.njbailey.explorer.tree;
 
 import com.njbailey.bytelib.Method;
+import com.njbailey.explorer.controls.MethodInfo;
+import javafx.scene.Scene;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
 
 public class MethodTreeItem extends SimpleTreeItem<Method> {
     public MethodTreeItem(Method method) {
@@ -12,7 +15,15 @@ public class MethodTreeItem extends SimpleTreeItem<Method> {
 
     @Override
     public ContextMenu getContextMenu() {
-        MenuItem menu = new MenuItem("Example");
+        MenuItem menu = new MenuItem("GetInfo");
+        menu.setOnAction(e -> {
+            MethodInfo methodInfo = new MethodInfo(getData());
+            Scene scene = new Scene(methodInfo, 400, 600);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        });
+
         return new ContextMenu(menu);
     }
 }
