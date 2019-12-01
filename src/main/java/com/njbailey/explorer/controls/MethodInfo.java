e1608ea5f057d2346834808142908866b3958180
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2019-11-30 21:15:57 -0500

    Display method descriptor information. Add ability to edit descriptor. Backing functionality needs added. #3

--- src/main/java/com/njbailey/explorer/controls/MethodInfo.java
+++ src/main/java/com/njbailey/explorer/controls/MethodInfo.java
@@ -11,10 +11,13 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import javafx.scene.paint.Color;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.MethodNode;
 
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 public class MethodInfo extends VBox {
     private MethodNode method;
@@ -24,15 +27,82 @@ public class MethodInfo extends VBox {
         setPadding(new Insets(15.0));
         setSpacing(10.0);
 
-        HBox hbox = new HBox();
-        hbox.setSpacing(15.0);
+        Node basicInfo = createBasicInfo();
+        Node modifierInfo = createModifierInfo();
+        getChildren().addAll(basicInfo, modifierInfo);
+    }
+
+    private Node createBasicInfo() {
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(15.0);
+        gridPane.setVgap(15.0);
 
         Label nameLabel = new Label("Name:");
-        Label nameEditableLabel = new Label(method.getName());
-        hbox.getChildren().addAll(nameLabel, nameEditableLabel);
+        Label nameValueLabel = new Label(method.getName());
+        GridPane.setConstraints(nameLabel, 0, 0);
+        GridPane.setConstraints(nameValueLabel, 1, 0);
 
-        Node modifierInfo = createModifierInfo();
-        getChildren().addAll(hbox, modifierInfo);
+        String descriptor = method.getDesc();
+        Type returnType = Type.getReturnType(descriptor);
+        Type[] argumentTypes = Type.getArgumentTypes(descriptor);
+
+        Label descriptorLabel = new Label("Descriptor:");
+        EditableLabel descriptorValueLabel = new EditableLabel(descriptor);
+        Label descriptorErrorLabel = new Label();
+        descriptorErrorLabel.getStyleClass().add("error-text");
+
+        GridPane.setConstraints(descriptorLabel, 0, 1);
+        GridPane.setConstraints(descriptorValueLabel, 1, 1);
+        GridPane.setConstraints(descriptorErrorLabel, 2, 1);
+
+        Label returnLabel = new Label("Return Type:");
+        Label returnValueLabel = new Label(returnType.getClassName());
+        GridPane.setConstraints(returnLabel, 1, 2);
+        GridPane.setConstraints(returnValueLabel, 2, 2);
+
+        Label argumentTypesLabel = new Label("Argument Types:");
+        Label argumentTypesValueLabel = new Label();
+
+        if(argumentTypes.length == 0) {
+            argumentTypesValueLabel.setText("None");
+        } else {
+            argumentTypesValueLabel.setText(Arrays.toString(getTypeNames(argumentTypes)));
+        }
+
+        descriptorValueLabel.setOnEditted(() -> {
+            try {
+                String newDescriptor = descriptorValueLabel.getText();
+                Type newReturnType = Type.getReturnType(newDescriptor);
+                Type[] newArgumentTypes = Type.getArgumentTypes(newDescriptor);
+
+                returnValueLabel.setText(newReturnType.getClassName());
+
+                if (argumentTypes.length == 0) {
+                    argumentTypesValueLabel.setText("None");
+                } else {
+                    argumentTypesValueLabel.setText(Arrays.toString(getTypeNames(newArgumentTypes)));
+                }
+
+                descriptorErrorLabel.setText("");
+
+                // TODO: Implement descriptor updating.
+                // method.setDescriptor(newDescriptor);
+            } catch(Exception ignored) {
+                descriptorErrorLabel.setText("Invalid descriptor format.");
+            }
+        });
+
+        GridPane.setConstraints(argumentTypesLabel, 1, 3);
+        GridPane.setConstraints(argumentTypesValueLabel, 2, 3);
+
+        gridPane.getChildren().addAll(
+                nameLabel, nameValueLabel,
+                descriptorLabel, descriptorValueLabel, descriptorErrorLabel,
+                returnLabel, returnValueLabel,
+                argumentTypesLabel, argumentTypesValueLabel
+        );
+
+        return gridPane;
     }
 
     private Node createModifierInfo() {
@@ -40,6 +110,7 @@ public class MethodInfo extends VBox {
         titledPane.setText("Modifiers");
 
         FlowPane flowPane = new FlowPane();
+
         flowPane.setHgap(15.0);
         flowPane.setVgap(15.0);
 
@@ -70,5 +141,15 @@ public class MethodInfo extends VBox {
         checkBox.setSelected(defaultValue);
         checkBox.setTooltip(new Tooltip(description));
         return checkBox;
+    }
+
+    private String[] getTypeNames(Type[] types) {
+        String[] typeNames = new String[types.length];
+
+        for(int i = 0; i < types.length; i++) {
+            typeNames[i] = types[i].getClassName();
+        }
+
+        return typeNames;
     }
 }
